60867e1808d5b562f7da3b5e52439a3d556c7fd6
avocadowob <karenni0105@gmail.com>
2025-11-06 11:39:00 +0800

    usa

---
+++ usa_office/data.js
@@ -0,0 +1,280 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "建築物後側",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.9421216064336697,
+        "pitch": 0.04070817612569044,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5083878195486875,
+          "pitch": 0.31901892460300196,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7874299127539715,
+          "pitch": -0.19736936137499939,
+          "title": "小細節",
+          "text": "如果仔細一看 這裡有一個minecraft的三叉戟"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "建築物後側旁",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -1.3891396087301828,
+        "pitch": 0.09413765729064494,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.01533884809475,
+          "pitch": 0.3283151284445829,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.035788633403035064,
+          "pitch": 0.3320045396306668,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.159074381720213,
+          "pitch": -0.10947500517209185,
+          "title": "苦力怕",
+          "text": "從這個角度還看的到苦力怕"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "建築物前",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -1.4425374030391254,
+        "pitch": 0.05084143047958101,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.061567972199235044,
+          "pitch": 0.17279882864112395,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -3.1273951665351127,
+          "pitch": 0.10282281258495374,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4109122162778185,
+          "pitch": -0.1398485756576413,
+          "title": "鐵劍",
+          "text": "如果放大超多就可以看到有一把鐵劍"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "建築物旁",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.6171009804192353,
+        "pitch": 0.0508852201571095,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.082565535920603,
+          "pitch": 0.2265795684451568,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.00294565651751455,
+          "pitch": 0.14046022467376318,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8829541755922392,
+          "pitch": -0.10139087690150461,
+          "title": "羊",
+          "text": "綠綠黃黃藍藍的三隻不同羊"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "停車場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.5870675545285442,
+        "pitch": 0.08806786349148155,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.014727257790713821,
+          "pitch": 0.1375569959954248,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 1.2593409101977464,
+          "pitch": 0.1805540992161383,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9373783788680967,
+          "pitch": -0.10005450240398339,
+          "title": "看不出來",
+          "text": "玻璃反光太嚴重看不出裡面的樣子"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
