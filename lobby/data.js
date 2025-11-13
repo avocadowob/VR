7c58d7f9bd3fc7a4b695b2794e85d52565edb076
avocadowob <karenni0105@gmail.com>
2025-11-13 11:52:15 +0800

    lobby

--- lobby/data.js
+++ lobby/data.js
@@ -1,38 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-pano",
-      "name": "pano",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
-      ],
-      "faceSize": 376.5,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.0386322500581322,
-          "pitch": 0.12178939863190053,
-          "rotation": 0,
-          "target": "1-bw"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-bw",
-      "name": "bw",
+      "id": "0-",
+      "name": "主大廳",
       "levels": [
         {
           "tileSize": 256,
@@ -54,7 +24,7 @@ var APP_DATA = {
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "lobby",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
