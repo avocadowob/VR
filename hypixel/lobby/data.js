32a7d1c8c0651e725d2265f529e5deab8a766f26
avocadowob <karenni0105@gmail.com>
2025-11-13 11:25:24 +0800

    lobby

---
+++ hypixel/lobby/data.js
@@ -0,0 +1,64 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano",
+      "name": "pano",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 376.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0386322500581322,
+          "pitch": 0.12178939863190053,
+          "rotation": 0,
+          "target": "1-bw"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bw",
+      "name": "bw",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 376.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
