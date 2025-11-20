1d90275f3d92446da78a5147c2875b92141a79ac
avocadowob <karenni0105@gmail.com>
2025-11-20 11:10:27 +0800

    add

---
+++ skywars/data.js
@@ -0,0 +1,285 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "大廳地獄門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 804,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04545032853023123,
+          "pitch": 0.1453194188996978,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "大廳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 804,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3993948287667841,
+          "pitch": -0.16276639687292338,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.5104223025894967,
+          "pitch": 0.11004991270083231,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -3.1344985865496806,
+          "pitch": -0.1508616722767222,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2742535201352165,
+          "pitch": 0.040777014432022085,
+          "title": "每日任務",
+          "text": "右鍵村民-任務使者:)"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "單人空島戰爭遊戲選擇",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 804,
+      "initialViewParameters": {
+        "yaw": -0.023152326812176938,
+        "pitch": -0.0483409591492574,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3160126280067814,
+          "pitch": -0.0888842523990121,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.15354424419573043,
+          "pitch": 0.04540232236270114,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 2.4354220311004946,
+          "pitch": 0.18204661528553423,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5257898663650344,
+          "pitch": -0.04280114354668996,
+          "title": "Insane Skywars",
+          "text": "物資會較好"
+        },
+        {
+          "yaw": -0.13402094318431423,
+          "pitch": -0.12896158900338506,
+          "title": "Normal Skywars",
+          "text": "物資較為普通"
+        },
+        {
+          "yaw": -0.6582092139595179,
+          "pitch": -0.04827613844194012,
+          "title": "Mini Skywars",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "靈魂祠井",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 804,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.921754139205216,
+          "pitch": 0.19686459540204382,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.4205642698704555,
+          "pitch": 0.21081694095589398,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.021371378595848256,
+          "pitch": -0.06870886932493292,
+          "title": "靈魂祠井",
+          "text": "可以抽職業"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "開始遊戲",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 804,
+      "initialViewParameters": {
+        "yaw": -0.060537195207455596,
+        "pitch": 0.05850367514374,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7922830518585897,
+          "pitch": -0.1173505706281972,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.06240472489414195,
+          "pitch": 0.3837504788124484,
+          "title": "物資箱",
+          "text": "裡面有方塊，裝備，武器，等物資"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "等待開始遊戲",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 804,
+      "initialViewParameters": {
+        "yaw": -0.026721390891999874,
+        "pitch": 0.09667167860425607,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20043608510036037,
+          "pitch": 1.32780332261785,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "空島戰爭",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
